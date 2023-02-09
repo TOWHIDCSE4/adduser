5d057eb834ff62200aee35343f2e94e021b36a01
safwansadi <safwaniftekher@gmail.com>
2023-02-09 18:56:29 +0600

    update create-user ui from email

--- src/pages/users/createTemp.tsx
+++ src/pages/users/createTemp.tsx
@@ -6,105 +6,108 @@ import to from "await-to-js";
 import useBaseHook from "@src/hooks/BaseHook";
 import { LeftCircleFilled, SaveFilled } from "@ant-design/icons";
 import UserDoneForm from "@root/src/components/Admin/Users/UserInformationForm";
-import Layout from '@src/layouts/Login'
-
+import Layout from "@src/layouts/Login";
 
 // const Layout = dynamic(() => import("@src/layouts/Admin"), { ssr: false });
 
 const CreateTemp = () => {
-	const { t, notify, redirect, router } = useBaseHook();
-	const [loading, setLoading] = useState(false);
-	// const [token, setToken]: any[] = useState();
-	const [form] = Form.useForm();
-	const { query } = router
-	const token = query.token
-	console.log("token", token)
-	//submit form
-	const onFinish = async (values: any): Promise<void> => {
-		setLoading(true);
-		let data = { token: token, ...values }
-		// let { rePassword, ...otherValues } = values;
-		console.log("data",data)
-		let [error, result]: any[] = await to(
-			userTempsService().updateUserTemp({data}));
+  const { t, notify, redirect, router } = useBaseHook();
+  const [loading, setLoading] = useState(false);
+  // const [token, setToken]: any[] = useState();
+  const [form] = Form.useForm();
+  const { query } = router;
+  const token = query.token;
+  console.log("token", token);
+  //submit form
+  const onFinish = async (values: any): Promise<void> => {
+    setLoading(true);
+    let data = { token: token, ...values };
+    // let { rePassword, ...otherValues } = values;
+    console.log("data", data);
+    let [error, result]: any[] = await to(
+      userTempsService().updateUserTemp({ data })
+    );
 
-		setLoading(false);
-		if (error) return notify(t(`errors:${error.code}`), "", "error");
-		notify(t("messages:message.recordUserCreated"));
-		redirect("frontend.admin.login");
-		return result;
-	};
+    setLoading(false);
+    if (error) return notify(t(`errors:${error.code}`), "", "error");
+    notify(t("messages:message.recordUserCreated"));
+    redirect("frontend.admin.login");
+    return result;
+  };
 
-	const randompass = () => {
-		let result = "";
-		let characters =
-			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		let charactersLength = characters.length;
-		for (let i = 0; i < 8; i++) {
-			result += characters.charAt(
-				Math.floor(Math.random() * charactersLength)
-			);
-		}
-		return result;
-	};
+  const randompass = () => {
+    let result = "";
+    let characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  };
 
-	return (
-		<>
-			<div className="content">
-				<Form
-					form={form}
-					name="createAdmin"
-					layout="vertical"
-					initialValues={{
-						username: "",
-						password: randompass(),
-						email: "",
-						groupId: undefined,
-						tags: [],
-					}}
-					onFinish={onFinish}
-					scrollToFirstError
-				>
-					<Row>
-						<Col md={{ span: 16, offset: 4 }}>
-							<UserDoneForm form={form} isEdit={false} />
-							<Form.Item
-								wrapperCol={{ span: 24 }}
-								className="text-center"
-							>
-								<Button
-									onClick={() => router.back()}
-									className="btn-margin-right"
-								>
-									<LeftCircleFilled /> {t("buttons:back")}
-								</Button>
-								<Button
-									type="primary"
-									htmlType="submit"
-									loading={loading}
-									className="btn-margin-right"
-								>
-									<SaveFilled /> {t("buttons:submit")}
-								</Button>
-							</Form.Item>
-						</Col>
-					</Row>
-				</Form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="content-form">
+        <div className="logo"></div>
+        <div className="form-registration" id="registration">
+          <div className="content-form-login">
+            <div className="sitename-title">Create a new account</div>
+            <div className="sitename">
+              Fill out the information to create a new account
+            </div>
+          </div>
+          <Form
+            form={form}
+            name="createAdmin"
+            layout="vertical"
+            initialValues={{
+              username: "",
+              password: randompass(),
+              email: "",
+              groupId: undefined,
+              tags: [],
+            }}
+            onFinish={onFinish}
+            scrollToFirstError
+          >
+            <Row>
+              <Col md={{ span: 16, offset: 4 }}>
+                <UserDoneForm form={form} isEdit={false} />
+                <Form.Item wrapperCol={{ span: 24 }} className="text-center">
+                  <Button
+                    onClick={() => router.back()}
+                    className="btn-margin-right"
+                  >
+                    <LeftCircleFilled /> {t("buttons:back")}
+                  </Button>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    loading={loading}
+                    className="btn-margin-right"
+                  >
+                    <SaveFilled /> {t("buttons:submit")}
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 CreateTemp.Layout = (props) => {
-	const { t } = useBaseHook();
-	return (
-		<Layout
-			title={t("pages:users.create.title")}
-			description={t("pages:users.create.description")}
-			{...props}
-		/>
-	);
+  const { t } = useBaseHook();
+  return (
+    <Layout
+      title={t("pages:users.create.title")}
+      description={t("pages:users.create.description")}
+      {...props}
+    />
+  );
 };
-  
 
 export default CreateTemp;
